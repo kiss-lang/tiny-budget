752fdc43fe600665a52c17b44d1050aa728f86e2
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-11-14 13:23:30 -0700

    proof-of-concept svg line

--- src/tiny_budget/ServerMain.hx
+++ src/tiny_budget/ServerMain.hx
@@ -2,8 +2,8 @@ package tiny_budget;
 
 enum Record {
     MoneyAsOf(time:Date, amount:Float);
-    MoneyOver(start:Date, end:Date, amount:Float);
-    RepeatMoneyOver(startDayOfMonth:Int, endDayOfMonth:Int, amount:Float);
+    MoneyOver(start:Date, end:Date, amount:Float, ?asOf:Date);
+    RepeatMoneyOver(startDayOfMonth:Int, endDayOfMonth:Int, amount:Float, ?asOfDayOfMonth:Int);
 }
 
 class ServerMain {
