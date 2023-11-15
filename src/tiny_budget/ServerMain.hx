97a65f80ab53f5cd5ffb239afbddf431a77ddf35
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-11-14 22:21:55 -0700

    repeated monthly charges/credits

--- src/tiny_budget/ServerMain.hx
+++ src/tiny_budget/ServerMain.hx
@@ -2,8 +2,9 @@ package tiny_budget;
 
 enum Record {
     MoneyAsOf(time:Date, amount:Float);
-    MoneyOver(start:Date, end:Date, amount:Float, ?asOf:Date);
+    MoneyOver(start:Date, end:Date, amount:Float, ?asOf:Date, ?fromLabel:String);
     RepeatMoneyOver(startDayOfMonth:Int, endDayOfMonth:Int, amount:Float, ?asOfDayOfMonth:Int);
+    // TODO MoneyWeekly
 }
 
 class ServerMain {
